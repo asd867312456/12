5ae3b427a205a48ddceda3aa165dd91b6a2c6efb
asd867312456 <867312456@qq.com>
2022-01-11 16:52:41 +0800

    1

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     now_time = time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time()))
     # allure 测试报告路径
     cur_path = os.path.dirname(os.path.realpath(__file__))
-    report_path = os.path.join(cur_path, f'Report\\{now_time}')
+    
+    # report_path = os.path.join(cur_path, f'Report\\{now_time}')
+    #暂时定死
+    report_path = os.path.join(cur_path, f'Report\\123')
 
     # -s : 打印信息
     # -m ：运行含标签的用例

66a357edf517d40565d3b93f94416accd18b6c22
asd867312456 <867312456@qq.com>
2022-01-12 11:14:29 +0800

    测试生成报告

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import time
 import pytest
+import shutil
 
 if __name__ == '__main__':
     # 当前时间
@@ -11,10 +12,13 @@ if __name__ == '__main__':
     
     # report_path = os.path.join(cur_path, f'Report\\{now_time}')
     # #暂时定死
-    report_path = os.path.join(cur_path, "allure-report")
+    #运行前先删除再新增
+    shutil.rmtree("Report")
+    report_path = os.path.join(cur_path, "Report")
 
     # -s : 打印信息
     # -m ：运行含标签的用例
     pytest.main(["--alluredir", report_path])
     # 解析测试报告，执行: allure serve {report_path}
     os.system(f"allure serve {report_path}")
+
